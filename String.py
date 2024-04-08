50787a10184dbafc2fa4868026b1e6ae0551fa1d
mishalareekkan <mishalareekkan@gmail.com>
2024-04-08 12:02:57 +0530

    n

--- String.py
+++ String.py
@@ -1,22 +1,27 @@
-####   Input: num1 = "456", num2 = "77"
-####   Output: "533" 
-num1 = "456"
-num2 = "77"
-####   Output: "533"
-x = int(num1)+int(num2)
-print(x)
+# ####   Input: num1 = "456", num2 = "77"
+# ####   Output: "533" 
+# num1 = "456"
+# num2 = "77"
+# ####   Output: "533"
+# x = int(num1)+int(num2)
+# print(x)
 
-# All letters in this word are capitals, like "USA".
-# All letters in this word are not capitals, like "leetcode".
-# Only the first letter in this word is capital, like "Google".
+# # All letters in this word are capitals, like "USA".
+# # All letters in this word are not capitals, like "leetcode".
+# # Only the first letter in this word is capital, like "Google".
 
-class Solution:
-    def detectCapitalUse(self, word: str) -> bool:
-        if word.isupper():
-            return True
-        if word.islower():
-            return True
-        if word[0].isupper() and word[1:].islower():
-            return True
-        return False
+# class Solution:
+#     def detectCapitalUse(self, word: str) -> bool:
+#         if word.isupper():
+#             return True
+#         if word.islower():
+#             return True
+#         if word[0].isupper() and word[1:].islower():
+#             return True
+#         return False
 
+s = "hello world"
+r = ""
+for i in s:
+    r = i+r
+print(r)
--- test.py
+++ test.py
@@ -1,5 +1,5 @@
-l1 = [2,4,5]
-l2 = [5,6,5]
-total = [ x + y for x,y in zip(l1,l2)]
-total.reverse()
-print(total)
+# l1 = [2,4,5]
+# l2 = [5,6,5]
+# total = [ x + y for x,y in zip(l1,l2)]
+# total.reverse()
+# print(total)
