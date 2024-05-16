4518d3f16cad07305807e3555c41ae4f73a25fd2
mishalareekkan <mishalareekkan@gmail.com>
2024-05-16 15:35:47 +0530

    wwww

--- badge.py
+++ badge.py
@@ -1,26 +1,26 @@
-def squaring(x):
-    for i in x:
-        yield i **2
+# def squaring(x):
+#     for i in x:
+#         yield i **2
 
-x = squaring(l)
-print(x)
+# x = squaring(l)
+# print(x)
 
-def outer_function(a, b):
-    def inner_function(c, d):
-        return c + d
-    return inner_function(a, b)
+# def outer_function(a, b):
+#     def inner_function(c, d):
+#         return c + d
+#     return inner_function(a, b)
  
-result = outer_function(5, 10)
-print(result)
-def my_function(a):
-    if a < 40:
-        return
-        print("Terrible")
-    if a < 80:
-        return "Pass"
-    else:
-        return "Great"
-print(my_function(25))
+# result = outer_function(5, 10)
+# print(result)
+# def my_function(a):
+#     if a < 40:
+#         return
+#         print("Terrible")
+#     if a < 80:
+#         return "Pass"
+#     else:
+#         return "Great"
+# print(my_function(25))
 
-x = [i for i in range(0,1001) if i % 7 == 0]
-print(x)
+# x = [i for i in range(0,1001) if i % 7 == 0]
+# print(x)
